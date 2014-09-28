13b843c7caa424c7aeb925e924a6adc8b1b8026f
proknv <proknv@gmail.com>
2014-09-28 10:38:21 +0300

    Some improvements

--- src/test/java/collections/ImmutableCollectionsTest.java
+++ src/test/java/collections/ImmutableCollectionsTest.java
@@ -1,9 +1,10 @@
 package collections;
 
-import com.google.common.collect.ImmutableListMultimap;
-import com.google.common.collect.Multimap;
+import com.google.common.collect.*;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.Set;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -13,7 +14,10 @@ import static org.assertj.core.api.Assertions.*;
 public class ImmutableCollectionsTest {
 
     private Multimap<Integer, String> multimap;
+    private BiMap<Integer, String> biMap;
+    private Set<String> setOfValues;
 
+    //Immutable collections have handy builders which allows to fill up the collection in a fluent style
     @Before
     public void setup(){
         multimap = new ImmutableListMultimap.Builder<Integer, String>()
@@ -21,17 +25,48 @@ public class ImmutableCollectionsTest {
                 .putAll(2, "BBB", "CCC")
                 .putAll(3, "DDD", "EEE", "FFF")
                 .build();
+        biMap = new ImmutableBiMap.Builder<Integer, String>()
+                .put(1, "1")
+                .put(2, "2")
+                .put(3, "3")
+                .build();
+        setOfValues = new ImmutableSet.Builder<String>()
+                .add("AAA")
+                .add("BBB")
+                .add("CCC")
+                .build();
     }
 
+
     @Test
-    public void testCollectionCreation(){
+    public void testImmutableMultiMapCreation(){
         assertThat(multimap.get(1)).contains("AAA");
         assertThat(multimap.get(3)).containsOnly("DDD", "EEE", "FFF");
     }
 
+    @Test
+    public void testImmutableBiMapCreation(){
+        assertThat(biMap.get(1)).isEqualTo("1");
+    }
+
+    @Test
+    public void testImmutableSetCreation(){
+        assertThat(setOfValues).containsExactly("AAA", "BBB", "CCC");
+    }
+
     @Test(expected = UnsupportedOperationException.class)
-    public void testCollectionImmutability(){
+    public void testImmutableMultiMapImmutability(){
         multimap.put(4, "JJJ");
+    }
+
+    @Test(expected = UnsupportedOperationException.class)
+    public void testImmutableBiMapImmutability(){
+        biMap.put(4, "4");
+    }
+
+    @Test(expected = UnsupportedOperationException.class)
+    public void testImmutableSetImmutability(){
+        setOfValues.add("DDD");
     }
 
 }
--- src/test/java/collections/RangeTest.java
+++ src/test/java/collections/RangeTest.java
@@ -3,13 +3,19 @@ package collections;
 import com.google.common.base.Function;
 import com.google.common.base.Predicate;
 import com.google.common.base.Predicates;
+import com.google.common.collect.ContiguousSet;
+import com.google.common.collect.DiscreteDomain;
 import com.google.common.collect.Range;
+import com.google.common.primitives.Ints;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.*;
 
 /**
- *
+ *A Range defines the boundaries around a contiguous span of values of some
+ * Comparable type; for example, "integers from 1 to 100 inclusive." Note that it is not
+ * possible to iterate over these contained values. To do so, pass this range instance and
+ * an appropriate DiscreteDomain to ContiguousSet.create.
  */
 public class RangeTest {
 
@@ -27,7 +33,7 @@ public class RangeTest {
     }
 
 
-    public class Person {
+    public class Person implements Comparable<Person> {
         private String firstName;
         private String lastName;
         private int age;
@@ -36,29 +42,49 @@ public class RangeTest {
             return firstName;
         }
 
-        public void setFirstName(String firstName) {
+        public Person setFirstName(String firstName) {
             this.firstName = firstName;
+            return this;
         }
 
         public String getLastName() {
             return lastName;
         }
 
-        public void setLastName(String lastName) {
+        public Person setLastName(String lastName) {
             this.lastName = lastName;
+            return this;
         }
 
         public int getAge() {
             return age;
         }
 
-        public void setAge(int age) {
+        public Person setAge(int age) {
             this.age = age;
+            return this;
+        }
+
+        @Override
+        public int compareTo(Person o) {
+            return Ints.compare(age, o.getAge());
         }
     }
 
     @Test
     public void testPersonRange(){
+        Person person1 = new Person().setFirstName("John").setAge(17);
+        Person person2 = new Person().setFirstName("Michael").setAge(37);
+        Person person3 = new Person().setFirstName("Andrew").setAge(25);
+        Person person4 = new Person().setFirstName("Boris").setAge(41);
+        Range<Person> rangeOfPersons = Range.closed(person1, person2);
+        assertThat(rangeOfPersons.contains(person3)).isTrue();
+        assertThat(rangeOfPersons.contains(person4)).isFalse();
+    }
+
+    //Notice Range class implements Predicate interface
+    @Test
+    public void testPersonRangeAsPredicate(){
         //initializing
         Range<Integer> personAges = Range.closed(35, 50);
         Function<Person, Integer> ageFunction = new Function<Person, Integer>() {
